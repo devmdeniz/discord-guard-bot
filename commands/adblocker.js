4ef2ce7fe26ccda67a6e9d2e229b17bd0ff81221
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-26 01:51:55 +0300

    Adblocker command translated to English

--- commands/adblocker.js
+++ commands/adblocker.js
@@ -4,18 +4,18 @@ const Discord = require("discord.js");
 exports.run = async (bot, message, args) => {
   if (!args[0])
     return message.channel.send(
-      "aç ya da kapat (ac da dahildir) yazmalısın!"
+      "You must write 'open' or 'close' "
     );
   if (!message.member.hasPermission("MANAGE_GUILD"))
-    return message.channel.send("`SUNUCUYU_YÖNET` yetkisine sahip olmalısın!");
-  if (args[0] == "aç" || args[0] == "ac") {
-    const j = db.set(`reklam_${message.guild.id}`, "acik")
+    return message.channel.send("You must have `MANAGE_GUILD` Permission !");
+  if (args[0] == "open") {
+    const j = db.set(`ad_${message.guild.id}`, "opened")
       message.channel.send(
         "✅ Reklam Engelleme Sistemi başarıyla açıldı! Üyeleri Yasakla yetkisine sahip olanların reklamları engellenmicektir."
       );
   }
-  if (args[0] == "kapat") {
-    const j = db.set(`reklam_${message.guild.id}`, "kapali")
+  if (args[0] == "close") {
+    const j = db.set(`ad_${message.guild.id}`, "closed")
       message.channel.send(
         "✅  Reklam Engelleme Sistemi kapatıldı! Artık herkes reklam paylaşabilir yazabilir."
       );
@@ -24,12 +24,12 @@ exports.run = async (bot, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["reklamengel", "reklam-engelle"],
+  aliases: ["ad-block"],
   permLevel: 0
 };
 
 exports.help = {
-  name: "reklam-engelleme",
+  name: "adblock",
   description: "",
   usage: ""
 };
