6cc0c7ac2750fde2e5584f6e451d9c7d9241e3bc
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-26 02:11:23 +0300

    Swearing Blocker translated to English

--- commands/swearingblocker.js
+++ commands/swearingblocker.js
@@ -1,20 +1,21 @@
 const db = require("quick.db");
 const Discord = require("discord.js");
+const settings = require("../settings.json")
 exports.run = async (bot, message, args) => {
   if (!args[0])
     return message.channel.send(
-      "aç yada kapat yazmalısın! Örnek: küfür-engel aç"
+      "you can just open or close for ex: " + settings.prefix + "open/close"
     );
   if (!message.member.hasPermission("MANAGE_GUILD"))
-    return message.channel.send("`SUNUCUYU_YÖNET` yetkisine sahip olmalısın!");
-  if (args[0] == "aç" || args[0] == "ac") {
-    const i = db.set(`kufur_${message.guild.id}`, "acik")
+    return message.channel.send("You must have `MANAGE GUILD` permission!");
+  if (args[0] == "open" || args[0] == "close") {
+    const i = db.set(`swear_${message.guild.id}`, "opened")
       message.channel.send(
         "✅ Küfür Engelleme Sistemi başarıyla açıldı! Üyeleri Yasakla yetkisine sahip olanların küfürü engellenmicektir."
       );
   }
-  if (args[0] == "kapat") {
-    const i = db.set(`kufur_${message.guild.id}`, "kapali")
+  if (args[0] == "close") {
+    const i = db.set(`swear_${message.guild.id}`, "closed")
       message.channel.send(
         "✅ Küfür Engelleme Sistemi başarıyla kapatıldı! Artık herkes küfür yazabilir."
       );
@@ -23,12 +24,12 @@ exports.run = async (bot, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["küfürengel", "kufur-engelleme", "kufurengel"],
+  aliases: ["swearingblocker"],
   permLevel: 0
 };
 
 exports.help = {
-  name: "küfür-engelleme",
+  name: "swearblock",
   description: "",
   usage: ""
 };
